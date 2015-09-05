5335ae21243c1d90334dd57983bda7cc3e66c24e
robertoallende <rallende@gmail.com>
2015-09-05 19:28:26 -0300

    Improving UI.

--- app/src/main/java/edu/nanodegree/spotify/PlayerActivity.java
+++ app/src/main/java/edu/nanodegree/spotify/PlayerActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 public class PlayerActivity extends AppCompatActivity {
     protected static String SONG_NAME = "songName";
@@ -32,7 +33,6 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
-
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
 
         Intent intent = getIntent();
